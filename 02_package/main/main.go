035f8dc5530e01fb3274241eff7a6249d892e09a
Paulo Corcino <paulo@corcino.com.br>
2017-08-09 00:15:22 -0300

    02 pacotes

---
+++ 02_package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/paulocorcino/GoTraining/02_package/stringutil"
+)
+
+func main() {
+	fmt.Println(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println(stringutil.MyName)
+}
